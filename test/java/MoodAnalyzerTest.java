59d6ee33110c3dc5df6466ac1e489b8938edf6ef
Bhavesh-Kataria <katariabhavesh6102000@gmail.com>
2025-01-22 14:48:54 +0530

    Mood Analyzer TC 4 AND TC 5 added

--- test/java/MoodAnalyzerTest.java
+++ test/java/MoodAnalyzerTest.java
@@ -11,10 +11,15 @@ public class MoodAnalyzerTest {
     void testAnalyzeMood_GivenSadMoodMessage_ShouldReturnSAD(){
         //arrange
         String expectedMood = "Sad";
+        String actualMood = null;
         MoodAnalyzer m = new MoodAnalyzer("I am currently feeling very sad too much");
 
         //act
-        String actualMood = m.analyzeMood();
+        try{
+            actualMood = m.analyzeMood();
+        }catch (InvalidMoodException ime){
+            System.out.println(ime.getExceptionType());
+        }
 
         //assert
         assertEquals(expectedMood,actualMood,"provided message did not produce SAD mood as expected");
@@ -22,31 +27,68 @@ public class MoodAnalyzerTest {
 
     @Test
     @DisplayName("Testing Happy Mood Message returns Happy")
-    void testAnalyzeMood_GivenHappyMoodMessage_ShouldReturnHAPPY(){
+    void testAnalyzeMood_GivenHappyMoodMessage_ShouldReturnHAPPY() {
         //arrange
         String expectedMood = "Happy";
+        String actualMood = null;
         MoodAnalyzer m = new MoodAnalyzer("Today is a happy day");
 
         //act
-        String actualMood = m.analyzeMood();
+        try {
+            actualMood = m.analyzeMood();
+        } catch (InvalidMoodException ime) {
+            System.out.println(ime.getExceptionType());
+        }
+        //assert
+        assertEquals(expectedMood, actualMood, "provided message did not produce HAPPY mood as expected");
+    }
+
+//    @Test
+//    @DisplayName("Testing null Mood returns Happy")
+//    void testAnalyzeMood_GivenNull_ShouldReturnHAPPY(){
+//        //arrange
+//        String expectedMood = "Happy";
+//        MoodAnalyzer m = new MoodAnalyzer(null);
+//
+//        //act
+//        String actualMood = m.analyzeMood();
+//
+//        //assert
+//        assertEquals(expectedMood,actualMood,"provided null message did not produce HAPPY mood as expected");
+//    }
+
+    @Test
+    @DisplayName("Testing Null Mood Message Throws InvalidMoodException indicating NULL TYPE")
+    void testAnalyzeMood_GivenNullMoodMessage_ShouldThrowInvalidMoodException(){
+        //arrange
+        MoodAnalyzer m = new MoodAnalyzer(null);
+        ExceptionTypes et = ExceptionTypes.EMPTY_MESSAGE;
+        //act
+        try{
+            m.analyzeMood();
+        }catch (InvalidMoodException ime){
+            et = ime.getExceptionType();
+        }
 
         //assert
-        assertEquals(expectedMood,actualMood,"provided message did not produce HAPPY mood as expected");
+        assertEquals(ExceptionTypes.NULL_MESSAGE,et,"Should have caught Null message exception");
     }
 
     @Test
-    @DisplayName("Testing null Mood returns Happy")
-    void testAnalyzeMood_GivenNull_ShouldReturnHAPPY(){
+    @DisplayName("Testing Empty Mood Message Throws InvalidMoodException indicating Empty TYPE")
+    void testAnalyzeMood_GivenEmptyMoodMessage_ShouldThrowInvalidMoodException(){
         //arrange
-        String expectedMood = "Happy";
-        MoodAnalyzer m = new MoodAnalyzer(null);
-
+        MoodAnalyzer m = new MoodAnalyzer("");
+        ExceptionTypes et = ExceptionTypes.NULL_MESSAGE;
         //act
-        String actualMood = m.analyzeMood();
+        try{
+            m.analyzeMood();
+        }catch (InvalidMoodException ime){
+            et = ime.getExceptionType();
+        }
 
         //assert
-        assertEquals(expectedMood,actualMood,"provided null message did not produce HAPPY mood as expected");
+        assertEquals(ExceptionTypes.EMPTY_MESSAGE,et,"Should have caught Empty message exception");
     }
-
 
 }
