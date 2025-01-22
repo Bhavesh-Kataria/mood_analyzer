b2571337d8a20797dbc349345c266e612b0a9e32
Bhavesh-Kataria <katariabhavesh6102000@gmail.com>
2025-01-22 13:01:51 +0530

    Mood Analyzer TestCase2 added

--- test/java/MoodAnalyzerTest.java
+++ test/java/MoodAnalyzerTest.java
@@ -1,14 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MoodAnalyzerTest {
+    MoodAnalyzer m;
+
+    @BeforeEach
+    void setup(){
+        m = new MoodAnalyzer();
+    }
+
     @Test
     @DisplayName("Testing Sad Mood Message returns Sad")
     void testAnalyzeMood_GivenSadMoodMessage_ShouldReturnSAD(){
         //arrange
-        MoodAnalyzer m = new MoodAnalyzer();
         String expectedMood = "Sad";
 
         //act
@@ -16,5 +23,18 @@ public class MoodAnalyzerTest {
 
         //assert
         assertEquals(expectedMood,actualMood,"provided message did not produce SAD mood as expected");
+    }
+
+    @Test
+    @DisplayName("Testing Happy Mood Message returns Happy")
+    void testAnalyzeMood_GivenHappyMoodMessage_ShouldReturnHAPPY(){
+        //arrange
+        String expectedMood = "Happy";
+
+        //act
+        String actualMood = m.analyzeMood("Today is a happy day");
+
+        //assert
+        assertEquals(expectedMood,actualMood,"provided message did not produce HAPPY mood as expected");
     }
 }
